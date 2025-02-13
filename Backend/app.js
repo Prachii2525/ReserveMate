88325cb70e8de71cdc9394195ee427d8bd31f51e
Prachiii18 <prachijanjire@gmail.com>
2025-02-13 20:38:00 +0530

    Added the database file

--- Backend/app.js
+++ Backend/app.js
@@ -5,4 +5,14 @@ import dotenv from 'dotenv';
 const app = express ();
 dotenv.config({ path: "./config/config,env"});
 
+app.use(
+    cors({
+        origin:[process.env.FRONTEND_URL],
+        methods: ["POST"],
+        credentials: true,
+    })
+);
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 export default app;
