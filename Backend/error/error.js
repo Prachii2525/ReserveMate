a8f78739acf7f5160e5fa3635021fc342a0920f5
Prachiii18 <prachijanjire@gmail.com>
2025-02-13 21:39:20 +0530

    error handling

---
+++ Backend/error/error.js
@@ -0,0 +1,16 @@
+class ErrorHandler extends Errors {
+    constructor( message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+export const errorMiddleware = (err, reg, res, next) =>{
+    err.message = err.message || "Internal Server Error!";
+    err.statusCode = err.statusCode || 500;
+
+    return res.status(err.statusCode).json({
+        success: false,
+        message: err.message,
+    });
+}
+export default ErrorHandler
