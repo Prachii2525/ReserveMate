7e74e7704549c4961390a4c9f78005d3ded125d8
Prachiii18 <prachijanjire@gmail.com>
2025-02-24 11:38:58 +0530

    Chsnged the CORS origin

--- Backend/app.js
+++ Backend/app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://reserve-mate-pn.vercel.app/",
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type"],
         credentials: true,
